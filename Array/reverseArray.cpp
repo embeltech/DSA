ca3e4b814756da54c702c97a1717aedaa858709f
embeltech <56225547+embeltech@users.noreply.github.com>
2024-06-22 12:55:43 +0530

    Create reverseArray.cpp

---
+++ Array/reverseArray.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+void printArray(int arr[],int len)
+{
+    for(int i = 0; i <= len-1 ; i++)
+        std::cout<<arr[i]<<" ";
+    std::cout<<std::endl;
+    
+}
+void reverseArray(int arr[],int len)
+{
+    int start = 0;
+    int end = len-1;
+    int temp;
+    while(start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+void reverseArrayRecurssive(int arr[],int start, int end)
+{
+    int temp;
+    if(start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverseArrayRecurssive(arr,start+1,end-1);
+    }
+    else
+    {
+        return;
+    }
+}
+void reverseArrayAlgo(int arr[],int size)
+{
+    int * start = arr;
+    int * end = arr + size;
+    std::reverse(start,end);
+}
+int main()
+{
+    int a[]={1,2,3,4,5,6,7,8,9,10,11};
+    int size = sizeof(a)/sizeof(a[0]);
+    std::cout<<"Hello World\n";
+    printArray(a,size);
+    //reverseArray(a,size);
+    //printArray(a,size);
+    
+    reverseArrayRecurssive(a,0,size-1);
+    printArray(a,size);
+    
+    //reverseArrayAlgo(a,size);
+    //printArray(a,size);
+
+    return 0;
+}
